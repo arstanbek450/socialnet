c14c0d404f3b6b221994282d47cf1eb36e870178
arstanbek450 <arstanbek450@gmail.com>
2023-08-01 16:18:47 +0600

    my first srs

--- core/models.py
+++ core/models.py
@@ -9,3 +9,32 @@ class Profile(models.Model):
     )
     nickname = models.CharField(max_length=55)
     description = models.TextField(null=True, blank=True)
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('Опубликован', 'Опубликован'),
+        ('Не опубликован', 'Не опубликован'),
+    )
+
+    name = models.CharField(max_length=80)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='post_photos/')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    rating = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
