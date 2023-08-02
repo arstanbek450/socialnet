9ef59ef45cf78fec3bea1319c58c1b8d722a0e97
arstanbek450 <arstanbek450@gmail.com>
2023-08-02 10:25:23 +0600

    my second comment

--- socialnet/urls.py
+++ socialnet/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core.views import homepage
+from core.views import contacts
+from core.views import about_us
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage),
+    path('contacts/', contacts),
+    path('about_us/', about_us),
 ]
