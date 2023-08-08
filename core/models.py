b60f23ec5f91f76536c525c8678894dd37614862
arstanbek450 <arstanbek450@gmail.com>
2023-08-08 09:16:08 +0600

    vse chetko rabotat

--- core/models.py
+++ core/models.py
@@ -82,16 +82,28 @@ class Short(models.Model):
         return f'{self.video} - {self.created_at}'
 
 
+# class SavedPosts(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     post = models.ManyToManyField(Post, verbose_name='saved post', related_name='saved_posts')
+#
+#     class Meta:
+#         verbose_name = 'saved post'
+#         verbose_name_plural = 'saved posts'
+#
+#     def __str__(self):
+#         return f'{self.user}'
+
 class SavedPosts(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    post = models.ManyToManyField(Post, verbose_name='saved post', related_name='saved_posts')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    post = models.ManyToManyField(Post, verbose_name='Сохраненные посты', related_name='saved_posts')
 
     class Meta:
-        verbose_name = 'saved post'
-        verbose_name_plural = 'saved posts'
+        verbose_name = 'Сохраненный пост'
+        verbose_name_plural = 'Сохраненные посты'
 
     def __str__(self):
         return f'{self.user}'
+
 
 
 class Profile(models.Model):
